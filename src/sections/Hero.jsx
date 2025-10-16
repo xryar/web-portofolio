afcbdb286f9e505e29eb8ad1bf6cc53661462fe3
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-10-16 15:53:58 +0700

    adjust 3D model position and add loading indicator

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,15 @@
 import HeroText from "../components/HeroText"
 import ParallaxBackground from "../components/ParallaxBackground"
-import { Canvas } from "@react-three/fiber"
+import { Canvas, useFrame } from "@react-three/fiber"
 import { Astronaut } from "../components/Astronaut"
-import { OrbitControls } from "@react-three/drei"
+import { useMediaQuery } from "react-responsive"
+import { easing } from "maath"
+import { Float } from "@react-three/drei"
+import { Suspense } from "react"
+import Loader from "../components/Loader"
 
 const Hero = () => {
+  const isMobile = useMediaQuery({ maxWidth: 853 })
   return (
     <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
         <HeroText/>
@@ -16,13 +21,31 @@ const Hero = () => {
             height: "100vh"
           }}
         >
-            <Canvas>
-                <Astronaut />
-                <OrbitControls />
-            </Canvas>
+          <Canvas camera={{ position: [0, 1, 3] }}>
+            <Suspense fallback={<Loader />}>
+              <Float>
+                <Astronaut 
+                  scale={isMobile && 0.23} 
+                  position={isMobile && [0, -1.5, 0]} 
+                />
+              </Float>
+              <Rig />
+            </Suspense>
+          </Canvas>
         </figure>
     </section>
   )
+}
+
+function Rig() {
+  return useFrame((state, delta) => {
+    easing.damp3(
+      state.camera.position,
+      [state.mouse.x / 10, 1 + state.mouse.y / 10, 3],
+      0.5,
+      delta
+    )
+  })
 }
 
 export default Hero
