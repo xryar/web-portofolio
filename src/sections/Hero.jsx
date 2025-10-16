1da0146b131a88cefe4f6f7ac1d9da448bb1f672
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-10-16 12:09:15 +0700

    add canvas in hero section to add astronaut component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,23 @@
 import HeroText from "../components/HeroText"
 import ParallaxBackground from "../components/ParallaxBackground"
+import { Canvas } from "@react-three/fiber"
 
 const Hero = () => {
   return (
     <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
         <HeroText/>
         <ParallaxBackground/>
+        <figure 
+          className="absolute inset-0"
+          style={{ 
+            width: "100vw",
+            height: "100vh"
+          }}
+        >
+            <Canvas>
+
+            </Canvas>
+        </figure>
     </section>
   )
 }
