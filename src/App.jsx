5a02d3a0d3a15fa602e321ada7926cf594634409
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-10-16 10:16:50 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './sections/Navbar'
+
+const App = () => {
+  return (
+    <div className='container mx-auto max-w-7xl'>
+      <Navbar/>
+      {/* hero */}
+      {/* about */}
+      {/* projects */}
+      {/* experience */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  )
+}
+
+export default App
