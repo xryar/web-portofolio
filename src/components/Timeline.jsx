4f69ab476abe42d88d4f76ca7698fadedc38162b
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-10-24 13:03:01 +0700

    refactor timeline code part 1

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -36,22 +36,20 @@ export const Timeline = ({
             <div
               className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
               <div
-                className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
+                className="h-10 absolute -left-[15px] w-10 rounded-full bg-midnight flex items-center justify-center">
                 <div
-                  className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
+                  className="h-4 w-4 rounded-full bg-neutral-800 borderborder-neutral-700 p-2" />
               </div>
-              <h3
-                className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
-                {item.title}
-              </h3>
+              <div className="flex-col hidden gap-2 text-xl font-bold md:flex md:pl-20 md:text-4xl text-neutral-300">
+                <h3>{item.date}</h3>
+                <h3 className="text-3xl text-neutral-400">{item.title}</h3>
+                <h3 className="text-3xl text-neutral-500">{item.job}</h3>
+              </div>
             </div>
 
             <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3
-                className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
-                {item.title}
-              </h3>
-              {item.content}{" "}
+              <h3>{item.job}</h3>
+              <h3>{item.date}</h3>
             </div>
           </div>
         ))}
