1dded82b16a6228b16a54e53555104066e8c4498
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-10-24 14:36:17 +0700

    complete the testimonial section

--- src/sections/Testimonial.jsx
+++ src/sections/Testimonial.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { twMerge } from "tailwind-merge"
+import { Marquee } from "../components/Marquee"
+import { reviews } from "../constants"
 
-const Testimonial = () => {
+const firstRow = reviews.slice(0, reviews.length / 2)
+const secondRow = reviews.slice(reviews.length / 2)
+
+const ReviewCard = ({
+  img,
+  name,
+  username,
+  body,
+}) => {
   return (
-    <div>Testimonial</div>
+    <figure
+      className={twMerge(
+        "relative h-full w-64 cursor-pointer overflow-hidden rounded-xl border p-4 border-gray-50/[.1] bg-gradient-to-r bg-indigo to-storm hover:bg-royal hover-animation"
+      )}
+    >
+      <div className="flex flex-row items-center gap-2">
+        <img className="rounded-full bg-white/10" width="32" height="32" alt="" src={img} />
+        <div className="flex flex-col">
+          <figcaption className="text-sm font-medium text-white">
+            {name}
+          </figcaption>
+          <p className="text-xs font-medium text-white/40">{username}</p>
+        </div>
+      </div>
+      <blockquote className="mt-2 text-sm">{body}</blockquote>
+    </figure>
   )
 }
 
-export default Testimonial
+export default function Testimonial() {
+  return (
+    <div className="items-start mt-25 md:mt-35 c-space">
+        <h2 className="text-heading">Hear From My Clients</h2>
+        <div className="relative flex w-full flex-col items-center justify-center overflow-hidden mt-12">
+            <Marquee pauseOnHover className="[--duration:20s]">
+                {firstRow.map((review) => (
+                    <ReviewCard key={review.username} {...review} />
+                ))}
+            </Marquee>
+            <Marquee reverse pauseOnHover className="[--duration:20s]">
+                {secondRow.map((review) => (
+                    <ReviewCard key={review.username} {...review} />
+                ))}
+            </Marquee>
+            <div className="from-primary pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r"></div>
+            <div className="from-primary pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l"></div>
+        </div>
+    </div>
+  )
+}
