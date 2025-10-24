16cb917d79491c5c43d45ea87f0d39b1aa4a55d9
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-10-24 14:14:25 +0700

    setup testimonial section with marquee

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './sections/Hero'
 import About from './sections/About'
 import Projects from './sections/Projects'
 import Experiences from './sections/Experiences'
+import Testimonial from './sections/Testimonial'
 
 const App = () => {
   return (
@@ -12,7 +13,7 @@ const App = () => {
       <About />
       <Projects />
       <Experiences />
-      <section className='min-h-screen'></section>
+      <Testimonial />
       <section className='min-h-screen'></section>
       {/* contact */}
       {/* footer */}
