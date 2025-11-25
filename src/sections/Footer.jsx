d6b33fe495728257e22f359153f0720ff9a86cbe
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-11-25 15:15:10 +0700

    fix footer bug

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,22 +2,21 @@ import { mySocials } from "../constants"
 
 const Footer = () => {
   return (
-    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
-        <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full">
-            <div>
-                <p>Terms & Conditions</p>
-                <p>|</p>
-                <p>Privacy Policy</p>
-            </div>
-            <div className="flex gap-3">
-                {mySocials.map((social, index) => (
-                    <a href={social.href} key={index}>
-                        <img src={social.icon} className="w-5 h-5" alt={social.name} />
-                    </a>
-                ))}
-            </div>
-            <p>© 2025 Arya Rizki Andaru. All rights reserved.</p>
+    <section className="flex flex-wrap items-center mt-25 justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
+        <div className=" mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
+        <div className="flex gap-2">
+            <p>Terms & Conditions</p>
+            <p>|</p>
+            <p>Privacy Policy</p>
         </div>
+        <div className="flex gap-3">
+            {mySocials.map((social, index) => (
+                <a href={social.href} key={index}>
+                    <img src={social.icon} className="w-5 h-5" alt={social.name} />
+                </a>
+            ))}
+        </div>
+        <p>© 2025 Arya Rizki Andaru. All rights reserved.</p>
     </section>
   )
 }
